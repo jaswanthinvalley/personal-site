c04b6b5bd02eb4ea5d7ed77bf0f21653ca1afb89
jaswanthinvalley <jaswanthvenkat001@gmail.com>
2025-08-01 19:31:36 +0530

    data updated

--- src/assets/Components/Hero.jsx
+++ src/assets/Components/Hero.jsx
@@ -16,10 +16,10 @@ const Hero = () => {
           that people <span className='text-red-400'>love ❤️</span>
         </h2>
         <div className='social-icons flex mt-4 justify-center sm:justify-start'>
-          <a href='https://github.com/jaswanthinvally'>
+          <a href='https://github.com/jaswanthinvalley'>
             <FaGithub className='text-sky-500 transform hover:scale-125 transition-transform duration-300 text-2xl' />
           </a>
-          <a href='https://www.linkedin.com/in/jaswanth-v-754142212/'>
+          <a href='https://www.linkedin.com/in/jaswanth-venkatesan-754142212/'>
             <FaLinkedin className='text-sky-500 transform hover:scale-125 transition-transform duration-300 text-2xl mx-3' />
           </a>
           <a href='https://x.com/Jaswant23480367'>
@@ -32,10 +32,14 @@ const Hero = () => {
       </div>
       <div className='mt-4 sm:mt-0'>
         <img
-          className='h-60 rounded-md hover:scale-105 transition-transform duration-200 relative z-10 sm:h-40'
-          src='https://pbs.twimg.com/profile_images/1791828029683216384/PuKAhhXJ_400x400.jpg'
-          alt='Profile'
-        />
+  className="h-52 w-full object-cover object-top rounded-md hover:scale-105 transition-transform duration-200"
+  src="https://media.licdn.com/dms/image/v2/D5603AQHtvvWgXka0QA/profile-displayphoto-shrink_800_800/B56ZWaKPWbGUAc-/0/1742048132204?e=1756944000&v=beta&t=AZUiIaIkQ1eQBGTQ9QnkoB1iG7R0ai7eeRhmRJeVc2Q"
+  alt="Profile"
+/>
+
+
+
+
                 <button className='bg-sky-950 opacity-90 text-sky-500 p-3 lg:p-2 rounded-md mx-3 lg:mx-0 lg:me-2 my-4 font-poppins '>Resume</button>
                 <button className='bg-sky-950 opacity-90 text-sky-500 p-3 lg:p-2 rounded-md mx-3 lg:mx-0 my-4 font-poppins'>Hire me</button>
       </div>
